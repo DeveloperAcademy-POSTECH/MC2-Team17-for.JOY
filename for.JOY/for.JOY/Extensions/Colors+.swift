ac9525c8e56824e685be05a199de8387af25f4da
NewYorkKim <newyorkkim1030@gmail.com>
2023-09-23 17:58:56 +0900

    [Init] Change Color Name

--- for.JOY/for.JOY/Extensions/Colors+.swift
+++ for.JOY/for.JOY/Extensions/Colors+.swift
@@ -8,11 +8,11 @@
 import SwiftUI
 
 extension Color {
-    static let joyBlack = Color("JoyBlack")
-    static let joyWhite = Color("JoyWhite")
-    static let joyGrey300 = Color("JoyGrey300")
-    static let joyGrey200 = Color("JoyGrey200")
-    static let joyGrey100 = Color("JoyGrey100")
-    static let joyBlue = Color("JoyBlue")
-    static let joyYellow = Color("JoyYellow")
+    static let joyBlack = Color("forJoyBlack")
+    static let joyWhite = Color("forJoyWhite")
+    static let joyGrey300 = Color("forJoyGrey300")
+    static let joyGrey200 = Color("forJoyGrey200")
+    static let joyGrey100 = Color("forJoyGrey100")
+    static let joyBlue = Color("forJoyBlue")
+    static let joyYellow = Color("forJoyYellow")
 }
