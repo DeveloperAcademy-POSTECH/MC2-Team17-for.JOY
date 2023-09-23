fa9eaa7e6db6ee31a9a850153a7d88dd7ca98348
NewYorkKim <newyorkkim1030@gmail.com>
2023-09-23 15:12:28 +0900

    [Init] Extension Setting

---
+++ for.JOY/for.JOY/New Group/Colors+.swift
@@ -0,0 +1,18 @@
+//
+//  Colors+.swift
+//  for.JOY
+//
+//  Created by Nayeon Kim on 2023/09/23.
+//
+
+import SwiftUI
+
+extension Color {
+    static let joyBlack = Color("JoyBlack")
+    static let joyWhite = Color("JoyWhite")
+    static let joyGrey300 = Color("JoyGrey300")
+    static let joyGrey200 = Color("JoyGrey200")
+    static let joyGrey100 = Color("JoyGrey100")
+    static let joyBlue = Color("JoyBlue")
+    static let joyYellow = Color("JoyYellow")
+}
---
+++ for.JOY/for.JOY/New Group/Date+.swift
@@ -0,0 +1,19 @@
+//
+//  Date+.swift
+//  for.JOY
+//
+//  Created by Nayeon Kim on 2023/09/23.
+//
+
+import Foundation
+
+// Date 타입의 Extension으로 format에 따라 문자열로 반환하는 함수를 제공합니다.
+extension Date
+{
+    func toString(dateFormat format: String ) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = format
+        
+        return dateFormatter.string(from: self)
+    }
+}
